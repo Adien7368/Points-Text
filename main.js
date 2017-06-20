77fdbeffc50b9746b9fbafba0f7d6f23a939edcd
Adien7638 <avianshverma8922003966@gmail.com>
2017-06-20 16:58:59 +0530

    Text made from points in p5

---
+++ main.js
@@ -0,0 +1,21 @@
+
+var font;
+
+function preload(){
+	font = loadFont('spaceage.otf');
+}
+
+function setup(){
+	createCanvas(600,400);
+	background(51);
+	textFont(font);
+	text('Hi',100,100);
+}
+
+
+function draw(){
+
+
+
+}
+
